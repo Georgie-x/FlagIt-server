ba828c871a4beffae27700757b8a0f923e093967
Georgie <georgiex@gmail.com>
2025-11-01 16:02:38 +0000

    fixing up model

--- controller.ts
+++ controller.ts
@@ -1,6 +1,7 @@
-import { fetchHighSores, insetHighScores } from "./model"
+import { fetchHighScores, insertHighScores } from "./model"
+import { Request, Response, NextFunction } from "express"
 
-const getHighScores = async (req, res, next) => {
+const getHighScores = async (req : Request, res: Response, next: NextFunction) => {
 	try {
 		const { name, score, date } = req.query
 		const highscores = await fetchHighScores(name, score, date)
@@ -9,7 +10,7 @@ const getHighScores = async (req, res, next) => {
 		next(err)
 	}
 }
-const postHighScores = async (req, res, next) => {
+const postHighScores = async (req : Request, res: Response, next: NextFunction) => {
 	try {
 		const newScore = req.body
 		const { name, score, time } = req.params
