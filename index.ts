34ddf33a026e84f8e11f1f28b78888bea99d3e8b
Georgie <georgiex@gmail.com>
2025-11-19 21:59:17 +0000

    updated port

--- index.ts
+++ index.ts
@@ -1,4 +1,7 @@
 import app from "./app.js"
-const { PORT = 3000 } = process.env
 
-app.listen(PORT, () => console.log(`listening on ${PORT}...`))
+const PORT = Number(process.env.PORT) || 3000
+
+app.listen(PORT, "0.0.0.0", () => {
+	console.log(`listening on ${PORT}...`)
+})
